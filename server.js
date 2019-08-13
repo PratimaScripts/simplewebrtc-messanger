080f8509a249eb15ba5c5521d7b72e32826edcdc
pratimagautam977 <astute.yard@gmail.com>
2019-08-13 22:41:27 +0545

    Assigned the different port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.YOUR_PORT || process.env.PORT || 80;
 
 // Set public folder as root
 app.use(express.static('public'));
